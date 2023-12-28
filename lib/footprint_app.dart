c0bb2cb20adc5cdc1f4cdda7e115b7088efe8014
Aidar Khabibullin <aydar.kh@gmail.com>
2023-12-28 18:39:12 +0500

    Add FootprintApp as a main class of app

---
+++ lib/footprint_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FootprintApp extends StatefulWidget {
+  const FootprintApp({super.key});
+
+  @override
+  State<FootprintApp> createState() => _FootprintAppState();
+}
+
+class _FootprintAppState extends State<FootprintApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
