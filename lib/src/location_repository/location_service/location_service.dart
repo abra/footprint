05d2ade3a51432e1800ef81bb39323010280d07e
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-04 17:25:32 +0500

    Add documentation comments to LocationService() class

--- lib/src/location_repository/location_service/location_service.dart
+++ lib/src/location_repository/location_service/location_service.dart
@@ -2,15 +2,34 @@ import 'package:geolocator/geolocator.dart';
 
 import 'models/exceptions.dart';
 
+/// Provides access to the device's location.
 class LocationService {
+  /// Creates a [LocationService] with the given update interval.
+  ///
+  /// The [updateInterval] determines the desired update interval when
+  /// requesting continuous location updates using [getLocationUpdatesStream].
+  /// The default is 2 seconds if not specified.
   LocationService({
     Duration? updateInterval,
   }) : _updateInterval = updateInterval ?? const Duration(seconds: 2);
 
+  /// Whether location service is enabled on the device.
   static bool _serviceEnabled = false;
+
+  /// The current location permission status.
   static LocationPermission _permission = LocationPermission.denied;
+
+  /// The location service update interval.
   final Duration _updateInterval;
 
+  /// Checks if location services are enabled and location permission is granted.
+  ///
+  /// Throws a [ServiceDisabledLocationServiceException] if location services
+  /// are disabled.
+  /// Throws a [PermissionDeniedLocationServiceException] if location permission
+  /// is denied.
+  /// Throws a [PermissionsPermanentlyDeniedLocationServiceException] if
+  /// location permissions are permanently denied.
   Future<void> _checkServiceAndPermissions() async {
     _serviceEnabled = await Geolocator.isLocationServiceEnabled();
 
@@ -37,6 +56,11 @@ class LocationService {
     }
   }
 
+  /// Gets a stream of location updates based on the desired update interval.
+  ///
+  /// The update interval is set in the constructor.
+  /// Can throw a [PermissionDeniedLocationServiceException] or
+  /// [ServiceDisabledLocationServiceException].
   Stream<Position> getLocationUpdatesStream() async* {
     await _checkServiceAndPermissions();
 
@@ -53,6 +77,10 @@ class LocationService {
     }
   }
 
+  /// Gets the current location of the device.
+  ///
+  /// Can throw a [PermissionDeniedLocationServiceException] or
+  /// [ServiceDisabledLocationServiceException].
   Future<Position> determineLocation() async {
     await _checkServiceAndPermissions();
 
@@ -69,6 +97,9 @@ class LocationService {
     }
   }
 
+  /// Calculates the distance between two locations in meters.
+  ///
+  /// Throws [UnableCalculateDistanceLocationServiceException] on failure.
   Future<double> calculateDistance({
     required double startLatitude,
     required double startLongitude,
