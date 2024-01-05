bdc11fb6c4b04b0b138344d6aa8e94fd46f00e1e
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-05 23:08:49 +0500

    Delete PlacemarkRepository() class with related files

--- lib/src/placemark_repository/placemark_local_storage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:footprint/src/local_storage/local_storage.dart';
-
-class PlacemarkLocalStorage {
-  PlacemarkLocalStorage({
-    required this.localStorage,
-  });
-
-  final LocalStorage localStorage;
-}
--- lib/src/placemark_repository/placemark_service.dart
+++
@@ -1,3 +0,0 @@
-class PlacemarkService {
-  PlacemarkService();
-}
