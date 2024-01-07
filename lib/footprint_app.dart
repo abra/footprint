ef889f67c50d48c35080049b17cda2958b8b4a02
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-07 19:56:16 +0500

    Add changes to FootprintApp widget

--- lib/footprint_app.dart
+++ lib/footprint_app.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:footprint/src/features/splash_screen/splash_screen.dart';
 
-class FootprintApp extends StatefulWidget {
+class FootprintApp extends StatelessWidget {
   const FootprintApp({super.key});
 
   @override
-  State<FootprintApp> createState() => _FootprintAppState();
-}
-
-class _FootprintAppState extends State<FootprintApp> {
-  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        future: Future<void>.delayed(
+          const Duration(seconds: 2),
+        ),
+        builder: (BuildContext ctx, AsyncSnapshot<void> snapshot) {
+          return AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: snapshot.connectionState == ConnectionState.done
+                // TODO: Replace with HomeScreen widget
+                ? const Scaffold(
+                    body: Center(
+                      child: Placeholder(
+                        child: Center(
+                          child: Text('HomeScreen'),
+                        ),
+                      ),
+                    ),
+                  )
+                : const SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
