b173de4dd85700d1699f9e53a4c93fcbc7392de7
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-10 00:58:20 +0500

    Add HomeScreen/PageView widget with PageManager

---
+++ lib/src/components/page_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageManager {
+  static final PageController _pageController = PageController();
+
+  static PageController get pageController => _pageController;
+
+  static void goToPage(int pageIndex) {
+    _pageController.animateToPage(
+      pageIndex,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.linearToEaseOut,
+    );
+  }
+}
