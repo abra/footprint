633b180737453d3046e39b2b481646feff7c2255
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-10 15:19:36 +0500

    Add comments to PageManager class

--- lib/src/components/page_manager.dart
+++ lib/src/components/page_manager.dart
@@ -5,6 +5,9 @@ abstract class PageIndex {
   static const int routeList = 1;
 }
 
+/// Page controller for switching between pages at _HomeScreen of FootprintApp
+///
+/// This class should be shared because it is used in multiple places
 class PageManager {
   static final PageController _pageController = PageController(
     initialPage: PageIndex.map,
