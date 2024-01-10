561270ed96060129497cebfe2aa9425b97db9526
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-10 15:02:06 +0500

    Add PageManager class

--- lib/src/components/page_manager.dart
+++ lib/src/components/page_manager.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreenPageManager {
-  static final PageController _pageController = PageController();
+abstract class PageIndex {
+  static const int map = 0;
+  static const int routeList = 1;
+}
+
+class PageManager {
+  static final PageController _pageController = PageController(
+    initialPage: PageIndex.map,
+  );
 
   static PageController get pageController => _pageController;
 
