f673671bfc04e8474edb5ab9a8215d3fca9234ff
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-10 21:40:43 +0500

    Add WidgetsFlutterBinding.ensureInitialized() method

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:footprint/src/app/footprint_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const FootprintApp());
 }
