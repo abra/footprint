4cb07ad0b799bf45cd5ff323d3860848bf78f583
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-14 02:36:53 +0500

    Update map_view.dart

--- lib/src/features/map/map_view.dart
+++ lib/src/features/map/map_view.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_animations/flutter_map_animations.dart';
 import 'package:footprint/src/domain_models/location.dart';
-import 'package:footprint/src/features/map/map_view_notifier.dart';
 import 'package:latlong2/latlong.dart';
 
 import 'map_location_notifier.dart';
+import 'map_view_notifier.dart';
 
 class MapView extends StatefulWidget {
   const MapView({
