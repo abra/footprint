f62bf4e08b5d1674b2d2781b8063414761c2a1a3
Aidar Khabibullin <aydar.kh@gmail.com>
2024-01-14 14:16:53 +0500

    Rename color constants

--- lib/src/app/common/colors.dart
+++ lib/src/app/common/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
-const Color greenColor = Color(0xFF055C5C);
-const Color whiteColor = Color(0xFFE7E3E3);
-const Color grayColor = Color(0xFF47556B);
-const Color purpleColor = Color(0xFF6B36DC);
-const Color purpleColor2 = Color(0xFF885EE1);
+const Color darkCyan = Color(0xFF055C5C);
+const Color white = Color(0xFFE7E3E3);
+const Color grayBlue = Color(0xFF47556B);
+const Color darkPurple = Color(0xFF6B36DC);
+const Color lightPurple = Color(0xFF885EE1);
