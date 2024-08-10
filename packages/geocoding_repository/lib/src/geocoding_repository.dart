2e1e1a1f89a1e5af6925384be878a395688ebdce
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-10 21:20:37 +0500

    Integrage new exception and refactor geocoding methods

--- packages/geocoding_repository/lib/src/geocoding_repository.dart
+++ packages/geocoding_repository/lib/src/geocoding_repository.dart
@@ -1,14 +1,27 @@
+import 'package:domain_models/domain_models.dart';
+import 'package:geocoding_repository/src/mappers/placemark_to_domain.dart';
 import 'package:meta/meta.dart';
+import 'package:sqlite_storage/sqlite_storage.dart';
 
 import 'geocoding_service.dart';
 
 class GeocodingRepository {
   const GeocodingRepository({
+    required this.storage,
     @visibleForTesting GeocodingService? geocodingService,
   }) : _geocodingService = geocodingService ?? const GeocodingService();
 
   final GeocodingService _geocodingService;
+  final SqliteStorage storage;
 
-
-
+  Future<PlaceAddress> getLocationAddress(Location location) async {
+    final lat = location.latitude;
+    final lon = location.longitude;
+    try {
+      final placemarkList = await _geocodingService.getPlacemarkList(lat, lon);
+      return placemarkList.first.toDomainModel(lat, lon);
+    } catch (e) {
+      throw CouldNotGetPlaceAddressException();
+    }
+  }
 }
--- packages/geocoding_repository/lib/src/geocoding_service.dart
+++ packages/geocoding_repository/lib/src/geocoding_service.dart
@@ -3,7 +3,7 @@ import 'package:geocoding/geocoding.dart';
 class GeocodingService {
   const GeocodingService();
 
-  Future<List<Placemark>> getPlacemark(double lat, double lon) async {
+  Future<List<Placemark>> getPlacemarkList(double lat, double lon) async {
     return await placemarkFromCoordinates(lat, lon);
   }
 }
