cb4ef3274420d8257b17fd50cbf6d47e937f0b08
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-10 19:50:23 +0500

    Update placemark_to_domain
    - Added optional timestamp parameter

--- packages/geocoding_repository/lib/src/mappers/placemark_to_domain.dart
+++ packages/geocoding_repository/lib/src/mappers/placemark_to_domain.dart
@@ -1,19 +1,20 @@
-import 'package:geocoding/geocoding.dart';
 import 'package:domain_models/domain_models.dart';
+import 'package:geocoding/geocoding.dart';
 
 extension PlacemarkToDomain on Placemark {
-  PlaceAddress toDomainModel(double lat, double lon) => PlaceAddress(
-    timestamp: DateTime.now(),
-    latitude: lat,
-    longitude: lon,
-    name: name ?? '',
-    locality: locality ?? '',
-    administrativeArea: administrativeArea ?? '',
-    country: country ?? '',
-    postalCode: postalCode ?? '',
-    subAdministrativeArea: subAdministrativeArea ?? '',
-    subLocality: subLocality ?? '',
-    subThoroughfare: subThoroughfare ?? '',
-    thoroughfare: thoroughfare ?? '',
-  );
+  PlaceAddress toDomainModel(double lat, double lon, [DateTime? timeStamp]) =>
+      PlaceAddress(
+        timestamp: timeStamp ?? DateTime.now(),
+        latitude: lat,
+        longitude: lon,
+        name: name ?? '',
+        locality: locality ?? '',
+        administrativeArea: administrativeArea ?? '',
+        country: country ?? '',
+        postalCode: postalCode ?? '',
+        subAdministrativeArea: subAdministrativeArea ?? '',
+        subLocality: subLocality ?? '',
+        subThoroughfare: subThoroughfare ?? '',
+        thoroughfare: thoroughfare ?? '',
+      );
 }
