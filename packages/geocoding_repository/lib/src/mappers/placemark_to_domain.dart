ac9fa38130cfb410f711fe95244e51cd419e48dd
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-11 23:11:03 +0500

    Simplify Placemark to Domain model conversion

--- packages/geocoding_repository/lib/src/mappers/placemark_to_domain.dart
+++ packages/geocoding_repository/lib/src/mappers/placemark_to_domain.dart
@@ -2,19 +2,35 @@ import 'package:domain_models/domain_models.dart';
 import 'package:geocoding/geocoding.dart';
 
 extension PlacemarkToDomain on Placemark {
-  LocationAddress toDomainModel(double lat, double lon, [DateTime? timeStamp]) =>
-      LocationAddress(
-        timestamp: timeStamp ?? DateTime.now(),
-        latitude: lat,
-        longitude: lon,
-        name: name ?? '',
-        locality: locality ?? '',
-        administrativeArea: administrativeArea ?? '',
-        country: country ?? '',
-        postalCode: postalCode ?? '',
-        subAdministrativeArea: subAdministrativeArea ?? '',
-        subLocality: subLocality ?? '',
-        subThoroughfare: subThoroughfare ?? '',
-        thoroughfare: thoroughfare ?? '',
+  // TODO: Replace with a more thoughtful version of address drafting
+  LocationAddress toDomainModel() {
+    if (street != null) {
+      return LocationAddress(address: street);
+    }
+    if (subThoroughfare != null && thoroughfare != null) {
+      return LocationAddress(address: '$subThoroughfare, $thoroughfare');
+    }
+    if (subThoroughfare == null && thoroughfare != null) {
+      return LocationAddress(address: '$thoroughfare');
+    }
+    if (subLocality != null && locality != null) {
+      return LocationAddress(address: '$subLocality, $locality');
+    }
+    if (subLocality == null && locality != null) {
+      return LocationAddress(address: locality);
+    }
+    if (subAdministrativeArea != null && administrativeArea != null) {
+      return LocationAddress(
+        address: '$subAdministrativeArea, $administrativeArea',
       );
+    }
+    if (subAdministrativeArea == null && administrativeArea != null) {
+      return LocationAddress(address: administrativeArea);
+    }
+    if (country != null) {
+      return LocationAddress(address: country);
+    }
+
+    return LocationAddress(address: null);
+  }
 }
