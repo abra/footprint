a9cca6bdee62d35189e09963c840cb9722539f04
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-11 23:12:58 +0500

    Create `GeocodingCacheStorage` class

---
+++ packages/geocoding_repository/lib/src/geocoding_cache_storage.dart
@@ -0,0 +1,19 @@
+import 'package:sqlite_storage/sqlite_storage.dart';
+
+class GeocodingCacheStorage {
+  GeocodingCacheStorage({
+    required this.sqliteStorage,
+  });
+
+  final SqliteStorage sqliteStorage;
+
+  // TODO: Clear cache
+
+  // TODO: Add cache
+
+  // TODO: Get cache
+
+  // TODO: Remove cache
+
+  // TODO: Update cache
+}
