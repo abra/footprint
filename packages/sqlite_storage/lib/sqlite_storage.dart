d2ef2f10ef7161befae5b29fe308ea40b56e5a4b
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-16 17:37:29 +0500

    Refactor file structure and imports for exceptions in sqlite_storage
    - Moved `exceptions.dart` from the root directory to the `models`
      directory to better align with the project structure.
    - Updated import paths in `sqlite_storage.dart` and `database_helper.dart`
      to reflect the new location of `exceptions.dart`.

--- packages/sqlite_storage/lib/sqlite_storage.dart
+++ packages/sqlite_storage/lib/sqlite_storage.dart
@@ -1,4 +1,4 @@
-export 'src/exceptions.dart';
+export 'src/models/exceptions.dart';
 export 'src/sqlite_storage.dart';
 export 'src/models/route_dto.dart';
 export 'src/models/place_address_cm.dart';
--- packages/sqlite_storage/lib/src/database_helper.dart
+++ packages/sqlite_storage/lib/src/database_helper.dart
@@ -1,7 +1,7 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-import 'exceptions.dart';
+import 'models/exceptions.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper._internal();
