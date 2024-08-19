948b142c269cd2b9e81220930176a3d845261411
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-20 01:36:07 +0500

    Implement `ForegroundTaskServiceProvider` and update `pubspec.yaml`
    - Added `ForegroundTaskServiceProvider` class in `foreground_task_service_provider.dart`
      to manage and provide `ForegroundTaskService` instances throughout the widget tree.
    - Updated `pubspec.yaml` to include the `flutter` SDK as a dependency for the
      `foreground_task_service` package.

---
+++ packages/foreground_task_service/lib/src/foreground_task_service_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+import 'foreground_task_service.dart';
+
+class ForegroundTaskServiceProvider extends InheritedWidget {
+  final ForegroundTaskService foregroundTaskService;
+
+  ForegroundTaskServiceProvider({
+    required this.foregroundTaskService,
+    required super.child,
+  });
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => false;
+
+  static ForegroundTaskService of(BuildContext context) {
+    final provider = context
+        .dependOnInheritedWidgetOfExactType<ForegroundTaskServiceProvider>();
+    return provider!.foregroundTaskService;
+  }
+}
