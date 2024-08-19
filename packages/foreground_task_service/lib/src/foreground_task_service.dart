b1f97fd8272b752229c7c0de276f5a9d7752fd6d
Aidar Khabibullin <aydar.kh@gmail.com>
2024-08-20 01:28:03 +0500

    Add `ForegroundTaskService` class and initialization logic

---
+++ packages/foreground_task_service/lib/src/foreground_task_service.dart
@@ -0,0 +1,128 @@
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+
+class ForegroundTaskService {
+  const ForegroundTaskService._internal();
+
+  static const ForegroundTaskService _instance =
+      ForegroundTaskService._internal();
+
+  factory ForegroundTaskService() => _instance;
+
+  Future<void> initService() async {
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'footprint_foreground_service',
+        channelName: 'Foreground Service Notification',
+        channelDescription:
+            'This notification appears when the foreground service is running.',
+        channelImportance: NotificationChannelImportance.LOW,
+        priority: NotificationPriority.LOW,
+      ),
+      iosNotificationOptions: const IOSNotificationOptions(
+        showNotification: true,
+        playSound: false,
+      ),
+      foregroundTaskOptions: const ForegroundTaskOptions(
+        interval: 5000,
+        isOnceEvent: false,
+        autoRunOnBoot: true,
+        autoRunOnMyPackageReplaced: true,
+        allowWakeLock: true,
+        allowWifiLock: true,
+      ),
+    );
+  }
+
+  Future<void> startService() async {
+    if (await FlutterForegroundTask.isRunningService) {
+      await FlutterForegroundTask.restartService();
+    } else {
+      await FlutterForegroundTask.startService(
+        notificationTitle: 'Foreground Service is running',
+        notificationText: 'Tap to return to the app',
+        callback: startCallback,
+      );
+    }
+  }
+
+  Future<void> stopService() async {
+    await FlutterForegroundTask.stopService();
+  }
+
+  void sendDataToTask(dynamic data) {
+    FlutterForegroundTask.sendDataToTask(data);
+  }
+
+  void addTaskDataCallback(Function(dynamic) callback) {
+    FlutterForegroundTask.addTaskDataCallback(callback);
+  }
+
+  void removeTaskDataCallback(Function(dynamic) callback) {
+    FlutterForegroundTask.removeTaskDataCallback(callback);
+  }
+}
+
+// The callback function should always be a top-level function.
+@pragma('vm:entry-point')
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  FlutterForegroundTask.setTaskHandler(MyTaskHandler());
+}
+
+class MyTaskHandler extends TaskHandler {
+  int _count = 0;
+
+  // Called when the task is started.
+  @override
+  void onStart(DateTime timestamp) {
+    print('onStart');
+  }
+
+  // Called every [ForegroundTaskOptions.interval] milliseconds.
+  @override
+  void onRepeatEvent(DateTime timestamp) {
+    FlutterForegroundTask.updateService(notificationText: 'count: $_count');
+
+    // Send data to main isolate.
+    FlutterForegroundTask.sendDataToMain(_count);
+
+    _count++;
+  }
+
+  // Called when the task is destroyed.
+  @override
+  void onDestroy(DateTime timestamp) {
+    print('onDestroy');
+  }
+
+  // Called when data is sent using [FlutterForegroundTask.sendDataToTask].
+  @override
+  void onReceiveData(Object data) {
+    print('onReceiveData: $data');
+  }
+
+  // Called when the notification button is pressed.
+  @override
+  void onNotificationButtonPressed(String id) {
+    print('onNotificationButtonPressed: $id');
+  }
+
+  // Called when the notification itself is pressed.
+  //
+  // AOS: "android.permission.SYSTEM_ALERT_WINDOW" permission must be granted
+  // for this function to be called.
+  @override
+  void onNotificationPressed() {
+    FlutterForegroundTask.launchApp('/');
+    print('onNotificationPressed');
+  }
+
+  // Called when the notification itself is dismissed.
+  //
+  // AOS: only work Android 14+
+  // iOS: only work iOS 10+
+  @override
+  void onNotificationDismissed() {
+    print('onNotificationDismissed');
+  }
+}
