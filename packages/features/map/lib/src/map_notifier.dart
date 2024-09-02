c138263bbb3c8d544ff7e9b5c5c2e5f8e556ac89
Aidar Khabibullin <aydar.kh@gmail.com>
2024-09-02 20:31:34 +0500

    Refactor `MapNotifier` for clarity and encapsulation
    - Moved private fields and methods to the bottom of the class for better encapsulation.
    - Grouped public properties and methods at the top for easier access and readability.
    - Removed unnecessary comments and organized existing comments for clarity.

--- packages/features/map/lib/src/map_notifier.dart
+++ packages/features/map/lib/src/map_notifier.dart
@@ -27,68 +27,30 @@ class MapNotifier {
     log('init $hashCode', name: 'MapNotifier', time: DateTime.now());
   }
 
-  final ForegroundLocationService _foregroundLocationService;
-  final RoutesRepository _routesRepository;
-  final GeocodingManager _geocodingManager;
-  final Config _config;
+  // public properties
 
-  // map view config
   Config get viewConfig => _config;
-
-  // map location update notifier
   late final locationState = ValueNotifier<LocationState>(LocationLoading());
-
-  // map route recording notifiers
   final isRouteRecordingActive = ValueNotifier<bool>(false);
   final routePoints = ValueNotifier<List<LatLng>>([]);
-
-  // map address retrieval notifier
   final placeAddress = ValueNotifier<PlaceAddressState>(PlaceAddressLoading());
-
-  // map view notifiers
   late final zoomLevel = ValueNotifier<double>(_config.defaultZoom);
   late final markerSize = ValueNotifier<double>(_config.markerSize);
   late final polylineWidth = ValueNotifier<double>(_config.polylineWidth);
   late final isMapCentered = ValueNotifier<bool>(_config.mapCentered);
-  final Map<double, double> _polylineWidthCache = {};
-  final Map<double, double> _markerSizeCache = {};
 
-  // map view callbacks
+  // public callbacks
+
   void Function(double)? onZoomChanged;
   void Function(LocationDM)? onMapCentered;
+
+  // public methods
 
   void init() async {
     log('init', name: '$this', time: DateTime.now());
     _foregroundLocationService.onLocationUpdated = _handleLocationUpdate;
 
     _foregroundLocationService.onLocationUpdateError = _handleLocationError;
-  }
-
-  void _handleLocationUpdate(LocationDM location) {
-    log('_handleLocationUpdate', name: '$runtimeType', time: DateTime.now());
-    locationState.value = LocationUpdateSuccess(location: location);
-
-    onPlaceAddressUpdate.call(location);
-
-    // Center the map on the current location
-    if (isMapCentered.value && onMapCentered != null) {
-      onMapCentered!.call(location);
-    }
-
-    // Start route recording
-    if (isRouteRecordingActive.value) {
-      // TODO: Replace with routeRepository
-      // TODO: _routesRepository.addRoutePoint(location.toLatLng());
-      routePoints.value = [
-        ...routePoints.value,
-        location.toLatLng(),
-      ];
-    }
-  }
-
-  void _handleLocationError(Exception exception) {
-    log('_handleLocationError', name: '$runtimeType', time: DateTime.now());
-    locationState.value = LocationUpdateFailure(error: exception);
   }
 
   void dispose() {
@@ -108,28 +70,6 @@ class MapNotifier {
     onZoomChanged = null;
     onMapCentered = null;
   }
-
-  Future<void> _placeAddressUpdate(LocationDM location) async {
-    log('_placeAddressUpdate', name: '$runtimeType', time: DateTime.now());
-    try {
-      final placeAddressModel =
-          await _geocodingManager.getAddressFromCoordinates(location);
-      if (placeAddressModel != null) {
-        placeAddress.value = PlaceAddressSuccess(
-          address: placeAddressModel.address,
-        );
-      }
-    } on CouldNotGetPlaceAddressException catch (e) {
-      log(
-        'Could not get location address: $e',
-        name: 'MapNotifier',
-        time: DateTime.now(),
-      );
-      placeAddress.value = PlaceAddressFailure(error: e);
-    }
-  }
-
-  Function(LocationDM) get onPlaceAddressUpdate => _placeAddressUpdate;
 
   Future<void> startRouteRecording() async {
     log('startRouteRecording', name: '$runtimeType', time: DateTime.now());
@@ -163,28 +103,6 @@ class MapNotifier {
         onZoomChanged ?? (_) {},
       );
 
-  Future<void> _updateZoom(double newZoom, Function(double) callback) async {
-    log('_updateZoom', name: '$runtimeType', time: DateTime.now());
-    if (newZoom >= _config.minZoom && newZoom <= _config.maxZoom) {
-      zoomLevel.value = newZoom;
-      callback(zoomLevel.value);
-      _updateMarkerSize(zoomLevel.value);
-      _updatePolylineWidth(zoomLevel.value);
-    }
-  }
-
-  Future<void> toggleMapCenter(bool value) async {
-    log('toggleMapCenter', name: '$runtimeType', time: DateTime.now());
-    if (onMapCentered != null) {
-      isMapCentered.value = value;
-
-      if (locationState.value is LocationUpdateSuccess) {
-        final state = locationState.value as LocationUpdateSuccess;
-        onMapCentered!.call(state.location);
-      }
-    }
-  }
-
   void _updatePolylineWidth(double zoom) {
     log('_updatePolylineWidth', name: '$runtimeType', time: DateTime.now());
     if (_polylineWidthCache.containsKey(zoom)) {
@@ -197,6 +115,73 @@ class MapNotifier {
       maxValue: _config.polylineMaxWidth,
     );
     _polylineWidthCache[zoom] = polylineWidth.value;
+  }
+
+  // private fields
+
+  final ForegroundLocationService _foregroundLocationService;
+  final RoutesRepository _routesRepository;
+  final GeocodingManager _geocodingManager;
+  final Config _config;
+  final Map<double, double> _polylineWidthCache = {};
+  final Map<double, double> _markerSizeCache = {};
+
+  // private callbacks
+  void _handleLocationUpdate(LocationDM location) {
+    log('_handleLocationUpdate', name: '$runtimeType', time: DateTime.now());
+    locationState.value = LocationUpdateSuccess(location: location);
+
+    onPlaceAddressUpdate.call(location);
+
+    // Center the map on the current location
+    if (isMapCentered.value && onMapCentered != null) {
+      onMapCentered!.call(location);
+    }
+
+    // Start route recording
+    if (isRouteRecordingActive.value) {
+      // TODO: Replace with routeRepository
+      // TODO: _routesRepository.addRoutePoint(location.toLatLng());
+      routePoints.value = [
+        ...routePoints.value,
+        location.toLatLng(),
+      ];
+    }
+  }
+
+  void _handleLocationError(Exception exception) {
+    log('_handleLocationError', name: '$runtimeType', time: DateTime.now());
+    locationState.value = LocationUpdateFailure(error: exception);
+  }
+
+  Future<void> _placeAddressUpdate(LocationDM location) async {
+    log('_placeAddressUpdate', name: '$runtimeType', time: DateTime.now());
+    try {
+      final placeAddressModel =
+          await _geocodingManager.getAddressFromCoordinates(location);
+      if (placeAddressModel != null) {
+        placeAddress.value = PlaceAddressSuccess(
+          address: placeAddressModel.address,
+        );
+      }
+    } on CouldNotGetPlaceAddressException catch (e) {
+      log(
+        'Could not get location address: $e',
+        name: 'MapNotifier',
+        time: DateTime.now(),
+      );
+      placeAddress.value = PlaceAddressFailure(error: e);
+    }
+  }
+
+  Future<void> _updateZoom(double newZoom, Function(double) callback) async {
+    log('_updateZoom', name: '$runtimeType', time: DateTime.now());
+    if (newZoom >= _config.minZoom && newZoom <= _config.maxZoom) {
+      zoomLevel.value = newZoom;
+      callback(zoomLevel.value);
+      _updateMarkerSize(zoomLevel.value);
+      _updatePolylineWidth(zoomLevel.value);
+    }
   }
 
   void _updateMarkerSize(double zoom) {
@@ -224,6 +209,8 @@ class MapNotifier {
             (maxValue - minValue) /
             (_config.maxZoom - _config.minZoom);
   }
+
+  Function(LocationDM) get onPlaceAddressUpdate => _placeAddressUpdate;
 }
 
 class MapNotifierProvider extends InheritedWidget {
